3b95bb58faa737c0f699b3fc0ebac193472c3885
Ben Lister <belister@microsoft.com>
2021-10-12 17:12:30 -0400

    moved 'Start' button down, added script for a 'Back' button

--- GamepAInt_Unity/Assets/Scripts/ChangeScene.cs
+++ GamepAInt_Unity/Assets/Scripts/ChangeScene.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class ChangeScene : MonoBehaviour
 {
 
     public void Play()
     {
         SceneManager.LoadScene("ARArtScene");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("UI");
     }
 }
