3a75608df3634079fcf5db8b655c073c3d6a9465
Srishti Mishra <srmishr@microsoft.com>
2021-10-14 21:27:40 +0530

    updated frameManager to create and move the cube on a detected plane, can't attach image

--- GamepAInt_Unity/Assets/Scripts/FrameManager.cs
+++ GamepAInt_Unity/Assets/Scripts/FrameManager.cs
@@ -11,26 +11,54 @@ namespace GamePaint
         public ARRaycastManager arRaycastManager;
         public ARPlaneManager arPlaneManager;
         public GameObject framePrefab;
+        public Canvas canvas;
+        public Image img;
         // public Button resetButton;
 
         private bool frameCreated = false;
         private GameObject instantiatedFrameObject;
 
+
         private List<ARRaycastHit> arRaycastHits = new List<ARRaycastHit>();
 
-        /*
-        private void Awake()
-        {
-            resetButton.onClick.RemoveAllListeners();
-            resetButton.onClick.AddListener(() =>
-            {
-                DeleteCube(instantiatedFrameObject);
-            });
-        }
-        */
 
         void Update()
         {
+            if (Input.touchCount > 0)
+            {
+                var touch = Input.GetTouch(0);
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    if (Input.touchCount == 1)
+                    {
+                        //Rraycast Planes
+                        if (arRaycastManager.Raycast(touch.position, arRaycastHits))
+                        {
+                            var pose = arRaycastHits[0].pose;
+                            if (frameCreated)
+                            {
+                                instantiatedFrameObject.transform.position = pose.position;
+                            }
+                            else
+                            {
+                                CreateCube(pose.position);
+                                return;
+                            }
+                        }
+
+                        Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                        if (Physics.Raycast(ray, out RaycastHit hit))
+                        {
+                            if (hit.collider.tag == "frame")
+                            {
+                                DeleteCube(hit.collider.gameObject);
+                            }
+                        }
+                    }
+                }
+            }
+
+            /*
             if (Input.touchCount > 0)
             {
                 var touch = Input.GetTouch(0);
@@ -45,27 +73,43 @@ namespace GamePaint
                             {
                                 var pose = arRaycastHits[0].pose;
                                 CreateCube(pose.position);
-                                TogglePlaneDetection(false);
+                                // TogglePlaneDetection(false);
                                 return;
                             }
                         }
+                        else
+                        {
+                            var pose = arRaycastHits[0].pose;
+                            instantiatedFrameObject.transform.position = pose.position;
+                            return;
+                        }
 
                         Ray ray = Camera.main.ScreenPointToRay(touch.position);
-                        if (Physics.Raycast(ray, out RaycastHit hit))
-                        {
-                            if (hit.collider.tag == "frame")
-                            {
-                                PickImage();
-                            }
-                        }
+                        //if (Physics.Raycast(ray, out RaycastHit hit))
+                        //{
+                        //    if (hit.collider.tag == "frame")
+                        //    {
+                        //        PickImage();
+                        //    }
+                        //}
                     }
                 }
             }
+            */
+        }
+
+        private void CreateImage(Vector3 position)
+        {
+            instantiatedFrameObject = Instantiate(framePrefab, Vector3.zero, Quaternion.identity);
+            instantiatedFrameObject.transform.SetParent(canvas.transform, false);
+            // instantiatedFrameObject.position = position;
+            // instantiatedFrameObject.rectTransform.anchoredPosition = Vector3.zero;
         }
 
         private void CreateCube(Vector3 position)
         {
             instantiatedFrameObject = Instantiate(framePrefab, position, Quaternion.identity);
+            //instantiatedFrameObject.texture
             frameCreated = true;
             // resetButton.gameObject.SetActive(true);
         }
@@ -96,7 +140,7 @@ namespace GamePaint
             Destroy(cubeObject);
             // resetButton.gameObject.SetActive(false);
             frameCreated = false;
-            TogglePlaneDetection(true);
+            // TogglePlaneDetection(true);
         }
     }
 }
